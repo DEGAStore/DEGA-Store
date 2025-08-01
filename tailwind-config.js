121072348b4429e9c02656a3000178a7d7acbb33
DEGAStore <degastoremail@gmail.com>
2025-08-01 15:17:55 +0300

    Add files via upload

---
+++ tailwind-config.js
@@ -0,0 +1,24 @@
+// tailwind-config.js
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'altBlack': '#1b1919', // Main background color
+            },
+            screens: {
+                'xs': '480px', // Add xs size
+            },
+            fontFamily: {
+                archivo: ['Archivo', 'sans-serif'],
+            },
+            fontSize: {
+                'h1': '36px',
+                'h2': '30px',
+                'h3': '24px',
+                'h4': '20px',
+                'h5': '18px',
+                'h6': '16px',
+          },
+        },
+    }
+};
